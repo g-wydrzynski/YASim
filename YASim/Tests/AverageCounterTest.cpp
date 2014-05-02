7ef01c5c7375ed65e741eb8bb53fe4cf45f1adaa
g-wydrzynski <otter@konin.lm.pl>
2014-05-02 18:39:34 +0200

    refactored AverageCounterTest

--- YASim/Tests/AverageCounterTest.cpp
+++ YASim/Tests/AverageCounterTest.cpp
@@ -4,6 +4,7 @@
 
 #include <thread>
 #include <vector>
+#include <algorithm>
 
 using YASim::Util::AverageCounter;
 using YASim::Util::ScopedStoper;
@@ -11,21 +12,44 @@ using YASim::Util::ScopedStoper;
 struct AverageCounterTest : public testing::Test
 {
 	typedef std::vector< std::chrono::milliseconds::rep > mvec;
-	mvec vec;
-	AverageCounter< mvec > ac;
+	mvec vec; // underlying container for ac
+	AverageCounter< mvec > ac; // test object
+	std::vector< int > test_vector;// test data container - holds test data for ac
+	
+	void process(AverageCounter< mvec >& ac)
+	{
+		if(elements)
+			EXPECT_EQ( static_cast<double>(sum) / elements, ac.getAverage() );
+		else
+			EXPECT_EQ( 0.0, ac.getAverage() );
+		EXPECT_EQ( elements, vec.size() );
+		EXPECT_EQ( elements, ac.getElements() );
+
+		std::for_each(begin(test_vector), end(test_vector), [&](int el) {
+			++elements;
+			sum += el;
+			ac.push_back( el );
+			EXPECT_EQ( static_cast<double>(sum) / elements, ac.getAverage() );
+			EXPECT_EQ( elements, vec.size() );
+			EXPECT_EQ( elements, ac.getElements() );
+		});
+	}
 
 	AverageCounterTest() : ac(vec), testing::Test() {}
+	int elements;
+	int sum;
 	
 	virtual void SetUp() override 
 	{
+		test_vector.clear();
+		sum = 0;
+		elements = 0;
 	}
 };
 
 TEST_F(AverageCounterTest, IsInitializedCorrectly)
 {
-	EXPECT_EQ( 0.0, ac.getAverage() );
-	EXPECT_EQ( 0, vec.size() );
-	EXPECT_EQ( 0, ac.getElements() );
+	process(ac);
 }
 
 TEST_F(AverageCounterTest, IsInitializedCorrectlyWithContainer)
@@ -34,26 +58,14 @@ TEST_F(AverageCounterTest, IsInitializedCorrectlyWithContainer)
 	vec.push_back(5);
 	AverageCounter< mvec > ac(vec);
 
-	EXPECT_EQ( 4.5, ac.getAverage() );
-	EXPECT_EQ( 2, vec.size() );
-	EXPECT_EQ( 2, ac.getElements() );
-	ac.push_back( 2 );
-	EXPECT_EQ( 11.0/3, ac.getAverage() );
-	EXPECT_EQ( 3, vec.size() );
-	EXPECT_EQ( 3, ac.getElements() );
+	elements = vec.size();
+	std::for_each(begin(vec), end(vec), [&](int el) {
+		sum += el;
+	});
+	test_vector.push_back(2);
+	process(ac);
 }
 
-TEST_F(AverageCounterTest, PushesCorrectly)
-{
-	ac.push_back( 3 );
-	EXPECT_EQ( 3.0, ac.getAverage() );
-	EXPECT_EQ( 1, vec.size() );
-	EXPECT_EQ( 1, ac.getElements() );
-	ac.push_back( 4 );
-	EXPECT_EQ( 3.5, ac.getAverage() );
-	EXPECT_EQ( 2, vec.size() );
-	EXPECT_EQ( 2, ac.getElements() );
-}
 
 TEST_F(AverageCounterTest, WorksWithScoped)
 {
@@ -72,44 +84,27 @@ TEST_F(AverageCounterTest, WorksWithScoped)
 	EXPECT_EQ( 3, ac.getElements() );
 }
 
+TEST_F(AverageCounterTest, PushesCorrectly)
+{
+	test_vector.push_back(3);
+	test_vector.push_back(4);
+	process(ac);
+}
+
 TEST_F(AverageCounterTest, WorksWithHighFluctuations)
 {
-	int cnt = 0;
-	ac.push_back( 1 );
-	EXPECT_EQ( 1.0, ac.getAverage() );
-	EXPECT_EQ( ++cnt, vec.size() );
-	EXPECT_EQ( cnt, ac.getElements() );
-	ac.push_back( 41 );
-	EXPECT_EQ( 21, ac.getAverage() );
-	EXPECT_EQ( ++cnt, vec.size() );
-	EXPECT_EQ( cnt, ac.getElements() );
-	ac.push_back( 1 );
-	EXPECT_EQ( 43.0/3, ac.getAverage() );
-	EXPECT_EQ( ++cnt, vec.size() );
-	EXPECT_EQ( cnt, ac.getElements() );
-	ac.push_back( 996 );
-	EXPECT_EQ( 259.75, ac.getAverage() );
-	EXPECT_EQ( ++cnt, vec.size() );
-	EXPECT_EQ( cnt, ac.getElements() );
+	test_vector.push_back(1);
+	test_vector.push_back(41);
+	test_vector.push_back(1);
+	test_vector.push_back(996);
+	process(ac);
 }
 
 TEST_F(AverageCounterTest, WorksWithLowFluctuations)
 {
-	int cnt = 0;
-	ac.push_back( 51 );
-	EXPECT_EQ( 51.0, ac.getAverage() );
-	EXPECT_EQ( ++cnt, vec.size() );
-	EXPECT_EQ( cnt, ac.getElements() );
-	ac.push_back( 52 );
-	EXPECT_EQ( 51.5, ac.getAverage() );
-	EXPECT_EQ( ++cnt, vec.size() );
-	EXPECT_EQ( cnt, ac.getElements() );
-	ac.push_back( 51 );
-	EXPECT_EQ( 154.0/3, ac.getAverage() );
-	EXPECT_EQ( ++cnt, vec.size() );
-	EXPECT_EQ( cnt, ac.getElements() );
-	ac.push_back( 51 );
-	EXPECT_EQ( 205.0/4, ac.getAverage() );
-	EXPECT_EQ( ++cnt, vec.size() );
-	EXPECT_EQ( cnt, ac.getElements() );
+	test_vector.push_back(51);
+	test_vector.push_back(52);
+	test_vector.push_back(51);
+	test_vector.push_back(51);
+	process(ac);
 }
